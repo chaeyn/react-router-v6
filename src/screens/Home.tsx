c33fa48def765ec735326d53baffc8b57cd80f77
chaeyn <chaulfe@gmail.com>
2025-09-03 21:50:40 +0900

    useSearchParams

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { users } from "../db";
 
 export default function Home() {
+  const [readSearchParams, setSearchParams] = useSearchParams();
+  // console.log(readSearchParams.get("geo")); // get의 값
+  // console.log(readSearchParams.has("geo")); // geo의 존재여부
+  setTimeout(() => {
+    setSearchParams({ day: "today", tomorrow: "123" });
+  }, 3000);
   return (
     <h1>
       <ul>
