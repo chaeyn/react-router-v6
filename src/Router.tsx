f1489d8a600fdfaf6933924619d527e8df0deff2
chaeyn <chaulfe@gmail.com>
2025-09-03 17:10:06 +0900

    errorElement

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import About from "./screens/About";
 import Home from "./screens/Home";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
 
 const router = createBrowserRouter([
   {
@@ -11,12 +13,16 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+        errorElement: <ErrorComponent />,
+        // Home페이지에서 에러가 발생한 경우 ErrorComponent 반환
       },
       {
         path: "about",
         element: <About />,
       },
     ],
+    errorElement: <NotFound />,
+    // url과 매칭되지 않은 경우 NotFound 반환
   },
 ]);
 
